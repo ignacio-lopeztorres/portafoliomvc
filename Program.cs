794b8378ef44b804675e2e2b344a2f9778f971eb
ignacio <nacho@localhost.com>
2023-04-30 17:48:26 -0600

    add first template

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
+    name: "default", //ruteo convencional
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
