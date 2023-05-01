182eb2e8d838670a68e49a1a6f5cb7d88559670a
ignacio <nacho@localhost.com>
2023-05-01 15:46:14 -0600

    Principios de responsabilidad unica, limitando a que se ejecute una tarea a la vez

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
@@ -15,38 +16,10 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var repositorioProyecto = new RepositorioProyectos();
+            var proyectos = repositorioProyecto.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
-        }
-
-        private List<Proyecto> ObtenerProyectos() {
-            return new List<Proyecto>(){
-                new Proyecto {
-                    Titulo = "Amazon",
-                    Description = "E-Commerce realizado en ASP.NET Core",
-                    Link = "http://amazon.com",
-                    ImagenURL = "/imagenes/amazon.jpg"
-                },
-                new Proyecto {
-                    Titulo = "New York Times",
-                    Description = "Pagina de noticias en React",
-                    Link = "http://nytimes.com",
-                    ImagenURL = "/imagenes/New-York-Times.png"
-                },
-                new Proyecto {
-                    Titulo = "Reddit",
-                    Description = "Red Social para compartir en comunidades",
-                    Link = "http://reddit.com",
-                    ImagenURL = "/imagenes/reddit-01.jpg"
-                },
-                new Proyecto {
-                    Titulo = "Steam",
-                    Description = "Tienda en linea para comprar videojuegos",
-                    Link = "http://store.teampowered.com",
-                    ImagenURL = "/imagenes/steam.jpg"
-                }
-            };
         }
 
         public IActionResult Privacy()
