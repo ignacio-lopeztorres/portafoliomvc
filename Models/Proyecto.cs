ba9d904d7a74c8b62a377516de70de318c33df67
ignacio <nacho@localhost.com>
2023-05-01 15:07:57 -0600

    Listado de proyectos

---
+++ Models/Proyecto.cs
@@ -0,0 +1,11 @@
+﻿namespace Portafolio.Models
+{
+    public class Proyecto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Description { get; set; }
+        public string ImagenURL { get; set; }
+        public string Link { get; set; }
+    }
+}
