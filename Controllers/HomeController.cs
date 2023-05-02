b05e6dce17d56ba12682b4828328e07ace68508c
ignacio <nacho@localhost.com>
2023-05-01 21:02:04 -0600

    Restablecimiento de la aplicacion, eliminando los ejemplos del uso de logger

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,31 +7,18 @@ namespace Portafolio.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
-        private readonly IConfiguration configuration;
 
         //la inyecion de dependencias se da en el constructor de la clase para poder se usada dentro de la clase
-        public HomeController(ILogger<HomeController> logger,
-            IRepositorioProyectos repositorioProyectos,
-            IConfiguration configuration
+        public HomeController(
+            IRepositorioProyectos repositorioProyectos
             )
         {
-            _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
-            this.configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            //obtencion del valor desde el archivo de configuracion appsettings.development.json cuando se ejecuta la aplicacion en modo desarrollo y si es en produccion se toma toa la configuracion desde appsetting.json
-            var apellido = configuration.GetValue<string>("Apellido");
-            _logger.LogTrace("Este es un mensaje de log trace");
-            _logger.LogDebug("Este es un mensaje de debug");
-            _logger.LogInformation("Este es un mensaje de log information");
-            _logger.LogWarning("Este es un mensaje de log warnig");
-            _logger.LogError("Este es un mensaje de log error");
-            _logger.LogCritical("Este es un mensaje de log critical " + apellido);
             var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
 
             var modelo = new HomeIndexViewModel() { 
